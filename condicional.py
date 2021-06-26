989b69aab0803c227528634a274c05657beaf5c8
natalia de la ossa <natis_rojas01@hotmail.com>
2021-06-26 16:16:09 +0200

    Esto es un convertidor de modenas

---
+++ condicional.py
@@ -0,0 +1,43 @@
+menu = """
+Bienvenido al conversor de monedas 💲💲💵
+
+1 - Pesos Colombianos
+2 - Pesos Argentinos
+3 - Pesos Mexicanos
+
+Elige una opcion: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input("cuantos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("tienes $" + dolares + "dolares")
+
+elif opcion == 2:
+
+    pesos = input("cuantos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("tienes $" + dolares + "dolares")
+
+   
+elif opcion == 3:
+    pesos = input("cuantos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("tienes $" + dolares + "dolares")
+else:
+    print("ingresa una opcion correcta")
+
+
