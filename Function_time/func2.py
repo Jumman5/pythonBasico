073d6367c9263f320bc6e9717bcd0c8bcd138a7c
jummman5 <mhjummanan@gmail.com>
2021-07-30 13:51:56 +0200

    task about dictionary

---
+++ Function_time/func2.py
@@ -0,0 +1,8 @@
+"""Write a function that takes as an input a string containing a series of words separated 
+by spaces and builds a dictionary where the keys are the last letters of the words 
+in the string and the associated values are lists with the words ending with that letter.
+ The lists must be ordered lexicographically.
+For example, for the string "the only impossible journey is the one you never
+ begin" must return {'e':['the', 'impossible', 'the', 'one'], 'y':['only', 'journey'],
+ 's':['is'], 'u':['you'], 'r':['never'], 'n':['begin']}."""
+ 
